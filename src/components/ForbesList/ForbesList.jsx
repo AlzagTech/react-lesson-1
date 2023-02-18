92f166ccdc4484b95104da092daf02eaaa59df52
Heveliev-destress <tolik05.03@ukr.net>
2023-02-18 16:22:20 +0200

    add task 3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -7,7 +7,34 @@ import {
   TitleBottom,
   LeaderBoardProfiles,
 } from './ForbesList.styled';
+import PropTypes from 'prop-types';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({list}) => {
+  return (<LeaderBoard>
+    <BoardHeader>
+      <BoardTitle>
+        <TitleTop>Forbes</TitleTop>
+        <TitleBottom>Leader board</TitleBottom>
+      </BoardTitle>
+    </BoardHeader>
+  
+    <LeaderBoardProfiles>
+      {list.map(forb=><ForbesListItem  
+      key={forb.id}
+      avatar={forb.avatar} 
+      name={forb.name} 
+      capital={forb.capital} 
+      isIncrease={forb.isIncrease} />)}
+    </LeaderBoardProfiles>
+  </LeaderBoard>)
 };
+
+
+ForbesList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    capital: PropTypes.number.isRequired,
+    isIncrease: PropTypes.bool.isRequired
+})).isRequired
+}
